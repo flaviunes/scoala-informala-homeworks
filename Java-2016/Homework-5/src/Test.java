0127dbead38c6cba6c73d10b81a0c5d96b1bea75
Flaviu Nes <flaviunes@gmail.com>
2016-11-30 23:28:29 +0200

    deleted test class

--- Java-2016/Homework-5/src/Test.java
+++
@@ -1,8 +0,0 @@
-import com.sun.xml.internal.fastinfoset.util.StringArray;
-
-public class Test {
-public static void main(String[] args) {
-	String aString = "I have oneapple";
-	System.out.println(aString.substring(7, 7 + 4));
-}
-}
