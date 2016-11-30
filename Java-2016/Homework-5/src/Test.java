ce219a7550229804800920069fa00c18ed76d5ee
Flaviu Nes <flaviunes@gmail.com>
2016-11-30 23:26:58 +0200

    created new unit tests.

---
+++ Java-2016/Homework-5/src/Test.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.fastinfoset.util.StringArray;
+
+public class Test {
+public static void main(String[] args) {
+	String aString = "I have oneapple";
+	System.out.println(aString.substring(7, 7 + 4));
+}
+}
