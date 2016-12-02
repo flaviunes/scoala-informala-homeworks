cc5dd75fac7522490bc2a0df508a2025a346ddaf
Flaviu Nes <flaviunes@gmail.com>
2016-12-02 09:22:23 +0200

    Changed all "if" with "while"

--- Java-2016/Homework-5/src/StringUtility.java
+++ Java-2016/Homework-5/src/StringUtility.java
@@ -31,105 +31,104 @@ public class StringUtility {
 		int index = 0;
 		boolean contains = containsDigit(lowerString, "one");
 
-		if (contains == true) {
+		while (containsDigit(lowerString, "one")) {
 			index = lowerString.indexOf("one");
 			returnString = lowerString.replace("one", "1");
 			String oneRandomCase = s.substring(index, index + 3);
 			String oneDigit = returnString.substring(index, index + 1);
 			s = s.replace(oneRandomCase, oneDigit);
+			lowerString = s.toLowerCase();
 		}
-		lowerString = s.toLowerCase();
 
 		contains = containsDigit(lowerString, "two");
-		if (contains == true) {
+		while (containsDigit(lowerString, "two")) {
 			index = lowerString.indexOf("two");
 			returnString = lowerString.replace("two", "2");
 			String twoRandomCase = s.substring(index, index + 3);
 			String twoDigit = returnString.substring(index, index + 1);
 			s = s.replace(twoRandomCase, twoDigit);
+			lowerString = s.toLowerCase();
 		}
-		lowerString = s.toLowerCase();
 
 		contains = containsDigit(lowerString, "three");
-		if (contains == true) {
+		while (containsDigit(lowerString, "three")) {
 			index = lowerString.indexOf("three");
 			returnString = lowerString.replace("three", "3");
 			String threeRandomCase = s.substring(index, index + 5);
 			String threeDigit = returnString.substring(index, index + 1);
 			s = s.replace(threeRandomCase, threeDigit);
+			lowerString = s.toLowerCase();
 		}
-		lowerString = s.toLowerCase();
 
 		contains = containsDigit(lowerString, "four");
-		if (contains == true) {
+		while (containsDigit(lowerString, "four")) {
 			index = lowerString.indexOf("four");
 			returnString = lowerString.replace("four", "4");
 			String fourRandomCase = s.substring(index, index + 4);
 			String fourDigit = returnString.substring(index, index + 1);
 			s = s.replace(fourRandomCase, fourDigit);
+			lowerString = s.toLowerCase();
 		}
-		lowerString = s.toLowerCase();
 
 		contains = containsDigit(lowerString, "five");
-		if (contains == true) {
+		while (containsDigit(lowerString, "five")) {
 			index = lowerString.indexOf("five");
 			returnString = lowerString.replace("five", "5");
 			String fiveRandomCase = s.substring(index, index + 4);
 			String fiveDigit = returnString.substring(index, index + 1);
 			s = s.replace(fiveRandomCase, fiveDigit);
+			lowerString = s.toLowerCase();
 		}
-		lowerString = s.toLowerCase();
 
 		contains = containsDigit(lowerString, "six");
-		if (contains == true) {
+		while (containsDigit(lowerString, "six")) {
 			index = lowerString.indexOf("six");
 			returnString = lowerString.replace("six", "6");
 			String sixRandomCase = s.substring(index, index + 3);
 			String sixDigit = returnString.substring(index, index + 1);
 			s = s.replace(sixRandomCase, sixDigit);
+			lowerString = s.toLowerCase();
 		}
-		lowerString = s.toLowerCase();
 
 		contains = containsDigit(lowerString, "seven");
-		if (contains == true) {
+		while (containsDigit(lowerString, "seven")) {
 			index = lowerString.indexOf("seven");
 			returnString = lowerString.replace("seven", "7");
 			String sevenRandomCase = s.substring(index, index + 5);
 			String sevenDigit = returnString.substring(index, index + 1);
 			s = s.replace(sevenRandomCase, sevenDigit);
+			lowerString = s.toLowerCase();
 		}
-		lowerString = s.toLowerCase();
 
 		contains = containsDigit(lowerString, "eight");
-		if (contains == true) {
+		while (containsDigit(lowerString, "eight")) {
 			index = lowerString.indexOf("eight");
 			returnString = lowerString.replace("eight", "8");
 			String eightRandomCase = s.substring(index, index + 5);
 			String eightDigit = returnString.substring(index, index + 1);
 			s = s.replace(eightRandomCase, eightDigit);
+			lowerString = s.toLowerCase();
 		}
-		lowerString = s.toLowerCase();
 
 		contains = containsDigit(lowerString, "nine");
-		if (contains == true) {
+		while (containsDigit(lowerString, "nine")) {
 			index = lowerString.indexOf("nine");
 			returnString = lowerString.replace("nine", "9");
 			String nineRandomCase = s.substring(index, index + 4);
 			String nineDigit = returnString.substring(index, index + 1);
 			s = s.replace(nineRandomCase, nineDigit);
+			lowerString = s.toLowerCase();
 		}
-		lowerString = s.toLowerCase();
 
 		contains = containsDigit(lowerString, "zero");
-		if (contains == true) {
+		while (containsDigit(lowerString, "zero")) {
 			index = lowerString.indexOf("zero");
 			returnString = lowerString.replace("zero", "0");
 			String zeroRandomCase = s.substring(index, index + 4);
 			String zeroDigit = returnString.substring(index, index + 1);
 			s = s.replace(zeroRandomCase, zeroDigit);
+			lowerString = s.toLowerCase();
 		}
-
-		lowerString = s.toLowerCase();
 
 		return s;
 	}
