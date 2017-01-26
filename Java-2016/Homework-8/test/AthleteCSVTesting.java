e72d5f30430639c28b7a2d93d952f803e112674a
Flaviu Nes <flaviunes@gmail.com>
2017-01-26 12:45:14 +0200

    Created Test class and imported JUnit

---
+++ Java-2016/Homework-8/test/AthleteCSVTesting.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.List;
+
+import org.junit.Test;
+
+
+
+public class AthleteCSVTesting {
+	
+//	@Test
+//	public void whenParsingCSVList_withOneNovel_shouldReturnBookList_ofOneNovel() throws IOException {
+//		String csv = "Novel,J.R.R Tolkien,The Hobbit,FANTASY\n";
+//		StringReader sr = new StringReader(csv);
+//		try (CSVBookReader bookReader = new CSVBookReader(sr)) {
+//			List<Book> books = bookReader.readBooks();
+//			assertEquals(1, books.size());
+//			Novel expected = new Novel("J.R.R Tolkien", "The Hobbit", Genre.FANTASY);
+//			assertEquals(expected, books.get(0));
+//		}
+//	}
+
+}
